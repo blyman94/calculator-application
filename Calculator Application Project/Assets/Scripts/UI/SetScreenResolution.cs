59fd9c7037c906a39987c791d75096427325f24a
Brandon Lyman <blyman94@comcast.net>
2022-01-26 08:59:07 -0500

    Calculator Application V1.1.0
    + Textures, colors, music, and sound effects.

--- Calculator Application Project/Assets/Scripts/UI/SetScreenResolution.cs
+++ Calculator Application Project/Assets/Scripts/UI/SetScreenResolution.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 
 /// <summary>
-/// Ensures the resolution is set to the design resolution.
+/// Ensures the resolution is set to the design resolution. This script should
+/// only be activated during the Android build process.
 /// </summary>
 public class SetScreenResolution : MonoBehaviour
 {
     void Start()
     {
+#if UNITY_ANDROID
         Screen.SetResolution(1080, 1920, true);
+#endif
+
+#if UNITY_STANDALONE
+        Screen.SetResolution(720, 1280, false);
+#endif
+
+#if UNITY_WEBGL
+        Screen.SetResolution(360, 640, false);
+#endif
     }
 }
